d6449d9a3f7f1aaf4affc7efde4d218bad08b97a
m.khoshnam <mojtaba_kh@hotmail.com>
2024-08-14 15:42:32 +0330

    add configurations and entities

---
+++ OauthJWT/OauthJWT/Context/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OauthJWT.Entities;
+
+namespace OauthJWT.Context
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options)
+            :base(options) 
+        {
+            
+        }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
--- OauthJWT/OauthJWT/Program.cs
+++ OauthJWT/OauthJWT/Program.cs
@@ -1,4 +1,7 @@
 
+using Microsoft.EntityFrameworkCore;
+using OauthJWT.Context;
+
 namespace OauthJWT
 {
     public class Program
@@ -6,6 +9,7 @@ namespace OauthJWT
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
+            var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 
             // Add services to the container.
 
@@ -13,6 +17,8 @@ namespace OauthJWT
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
+            builder.Services.AddDbContext<ApplicationDbContext>(options =>
+             options.UseSqlServer(connectionString));
 
             var app = builder.Build();
 
