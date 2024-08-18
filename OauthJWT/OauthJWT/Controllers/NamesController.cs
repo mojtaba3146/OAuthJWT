dbb978d51457cc73e11ca8e3ea0bf63513ff185c
m.khoshnam <mojtaba_kh@hotmail.com>
2024-08-18 14:27:49 +0330

    add swager authorization and test conroller

---
+++ OauthJWT/OauthJWT/Controllers/NamesController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace OauthJWT.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class NamesController : ControllerBase
+    {
+        [HttpGet("[action]")]
+        [Authorize]
+        public async Task<IActionResult> GetNames()
+        {
+            var names = await 
+                Task.FromResult(new List<string>() { "Adam","Goli"});
+            return Ok(names);
+        }
+    }
+}
--- OauthJWT/OauthJWT/Program.cs
+++ OauthJWT/OauthJWT/Program.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
 using OauthJWT.Context;
 using OauthJWT.Services;
 using System.Text;
@@ -48,6 +49,38 @@ namespace OauthJWT
             });
 
             builder.Services.AddTransient<IJwtService, JwtService>();
+
+            builder.Services.AddSwaggerGen(c =>
+            {
+
+                c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                {
+                    Description="This Moji Site Use Bearer Token To Pass",
+                    Name="Authorization",
+                    In=ParameterLocation.Header,
+                    Type=SecuritySchemeType.ApiKey,
+                    Scheme="Bearer"
+                });
+
+                c.AddSecurityRequirement(new OpenApiSecurityRequirement()
+                {
+                    {
+                        new OpenApiSecurityScheme
+                        {
+                            Reference = new OpenApiReference
+                            {
+                                Type = ReferenceType.SecurityScheme,
+                                Id = "Bearer"
+                            },
+                            Scheme = "oauth2",
+                            Name = "Bearer",
+                            In = ParameterLocation.Header,
+                        },
+                        new List<string>()
+                    }
+                });
+
+            });
 
             var app = builder.Build();
 
